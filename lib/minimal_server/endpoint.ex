561be3dbf8e104adde495862194ed6737b21c640
ismarfernandes <ismarfernandes@outlook.com>
2021-01-05 22:43:49 -0300

    first commit

---
+++ lib/minimal_server/endpoint.ex
@@ -0,0 +1,32 @@
+defmodule MinimalServer.Endpoint do
+  use Plug.Router
+
+  alias MinimalServer.Router
+
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Poison
+  )
+
+  plug(:dispatch)
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts) do
+    Plug.Cowboy.http(__MODULE__, [])
+  end
+
+  forward("/bot", to: Router)
+
+  match _ do
+    send_resp(conn, 404, "Requested page not found! :(")
+  end
+end
